a5988df406d142c9b7a7f1101f1dca97200d1c02
Nikita Kozhevnikov <kozherka2@gmail.com>
2025-12-11 00:45:09 +0500

    added UserRegistration.java

---
+++ src/test/java/models/UserRegistration.java
@@ -0,0 +1,12 @@
+package models;
+
+import lombok.*;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class UserRegistration {
+    private String email;
+    private String password;
+}
